98f597c1bd1315091cda39aad5b8aa4982d90101
Allen <mail@allen.cool>
2025-10-14 10:56:14 +0800

    init code-message

--- response/response.go
+++ response/response.go
@@ -1,28 +1,28 @@
 package response
 
 import (
-	"github.com/allen-ping/go-zero-response/xerrors"
+	"github.com/allen-ping/go-zero-response/v2/xerrors"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
 )
 
 type Body struct {
-	Code int         `json:"code"`
-	Msg  string      `json:"msg"`
-	Data interface{} `json:"data,omitempty"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 func Response(r *http.Request, w http.ResponseWriter, resp interface{}, err error) {
 	body := &Body{
-		Code: 0,
-		Msg:  "OK",
-		Data: resp,
+		Code:    0,
+		Message: "OK",
+		Data:    resp,
 	}
 
 	e := xerrors.FromError(err)
 	if err != nil {
 		body.Code = e.Code
-		body.Msg = e.Msg
+		body.Message = e.Msg
 	}
 
 	httpx.OkJson(w, body)
