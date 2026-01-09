a57e36ef127b1a6c687f772909d7a8ba50863184
Allen <mail@allen.cool>
2026-01-09 14:17:36 +0800

    add biz code  metrics

--- response/response.go
+++ response/response.go
@@ -2,9 +2,21 @@ package response
 
 import (
 	"github.com/allen-ping/go-zero-response/v3/xerrors"
+	"github.com/zeromicro/go-zero/core/metric"
 	"github.com/zeromicro/go-zero/rest/httpx"
 	"net/http"
+	"strconv"
 )
+
+const serverNamespace = "http_server"
+
+var metricBizCode = metric.NewCounterVec(&metric.CounterVecOpts{
+	Namespace: serverNamespace,
+	Subsystem: "requests",
+	Name:      "biz_code_total",
+	Help:      "business response code counter",
+	Labels:    []string{"method", "path", "code"},
+})
 
 type Body struct {
 	Body interface{} `json:"body,omitempty"`
@@ -29,6 +41,7 @@ func Response(r *http.Request, w http.ResponseWriter, resp interface{}, err erro
 		body.Ret.Code = e.Code
 		body.Ret.Msg = e.Msg
 	}
+	metricBizCode.Inc(r.Method, r.URL.Path, strconv.Itoa(body.Ret.Code))
 
 	httpx.OkJson(w, body)
 }
